ad1e5c6e6ff1e3890d50dfc37949d7b806813a86
alejoalaniz <38295571+alejoalaniz@users.noreply.github.com>
2018-06-06 05:26:41 +0200

    Update Juego.java

--- Juego.java
+++ Juego.java
@@ -13,8 +13,45 @@ import javax.swing.JPanel;
 public class Juego extends JPanel {
 
     Pelota pelota = new Pelota(this);
+    Pelota segundaPelota = new Pelota(this);
+    Pelota tercerPelota = new Pelota(this);
     Raqueta raqueta = new Raqueta(this);
     Obstaculo obstaculo = new Obstaculo(this);
+    Obstaculo segundoObstaculo = new Obstaculo(this);
+    Obstaculo tercerObstaculo = new Obstaculo(this);
+    int moverSegundaPelota = 0;
+    int moverTercerPelota = 0;
+
+    public void paint(Graphics g) {
+        super.paint(g); // borra
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON); // bordes
+        raqueta.pintarRaqueta(g2d);
+        pelota.pintarPelota(g2d);
+        obstaculo.pintarObstaculo(g2d);
+        if (pelota.primerImpacto == 1) {
+            segundoObstaculo.pintarSegundoObstaculo(g2d);
+        }
+        if ((pelota.impactoEnSegundoObjeto == 1) || (segundaPelota.impactoEnSegundoObjeto == 1)) {
+            segundaPelota.pintarSegundaPelota(g2d);
+            moverSegundaPelota = 1;
+        }
+        if ((pelota.segundoImpactoEnSegundoObjeto == 1) || (segundaPelota.segundoImpactoEnSegundoObjeto == 1)) {
+            tercerPelota.pintarTerceraPelota(g2d);
+            moverTercerPelota = 1;
+        }
+    }
+
+    public void moverObjetos() {
+        pelota.moverPelota();
+        raqueta.moverRaqueta();
+        if (moverSegundaPelota == 1) {
+            segundaPelota.moverPelota();
+        }
+        if (moverTercerPelota == 1) {
+            tercerPelota.moverPelota();
+        }
+    }
 
     public Juego() {
         setBackground(Color.PINK);
@@ -33,24 +70,16 @@ public class Juego extends JPanel {
         setFocusable(true);
     }
 
-    public void moverObjetos() {
-        pelota.moverPelota();
-        raqueta.moverRaqueta();
-    }
-
-    public void paint(Graphics g) {
-        super.paint(g); // borra
-        Graphics2D g2d = (Graphics2D) g;
-        Graphics2D g2d1 = (Graphics2D) g;
-        Graphics2D g2d2 = (Graphics2D) g;
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON); // bordes
-        raqueta.pintarRaqueta(g2d1);
-        pelota.pintarPelota(g2d);
-        obstaculo.pintarObstaculo(g2d2);
-    }
-
     public void errar() {
         JOptionPane.showMessageDialog(this, "Game Over", "Game Over", JOptionPane.YES_NO_OPTION);
+        // agregar un duraste tantos segundos
+        pelota = null;
+        segundaPelota = null;
+        tercerPelota = null;
+        raqueta = null;
+        obstaculo = null;
+        segundoObstaculo = null;
+        tercerObstaculo = null;
         System.exit(ABORT);
     }
 
