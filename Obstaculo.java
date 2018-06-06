c0a87fde2bc1a615c482254de2570233d8e75fea
alejoalaniz <38295571+alejoalaniz@users.noreply.github.com>
2018-06-06 05:25:04 +0200

    Update Obstaculo.java

--- Obstaculo.java
+++ Obstaculo.java
@@ -1,15 +1,17 @@
-package juego;
-
-import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Obstaculo {
 
-    int posicionObstaculoX = ThreadLocalRandom.current().nextInt(2, 290);
+    int posicionObstaculoX = ThreadLocalRandom.current().nextInt(110, 250);
+    int posicionSegundoObstaculoX = ThreadLocalRandom.current().nextInt(30, 99);
+    int posicionTercerObstaculoX = ThreadLocalRandom.current().nextInt(100, 211);
     int ancho = 6, alto = 3;
-    int posicionObstaculoY = 400;
+    int anchoTercero = 33;
+    int posicionObstaculoY = 390;
+    int posicionSegundoObstaculoY = 403;
+    int posicionTercerObstaculoY = 10;
     Juego juegoObstaculo;
 
     public Obstaculo(Juego juego) {
@@ -17,11 +19,29 @@ public class Obstaculo {
     }
 
     public void pintarObstaculo(Graphics2D gObstaculo) {
-        gObstaculo.fillRect(posicionObstaculoX, posicionObstaculoY, ancho, alto);
         gObstaculo.setColor(Color.blue);
+        gObstaculo.fillRect(posicionObstaculoX, posicionObstaculoY, ancho, alto);
     }
 
-    public Rectangle getBounds() {
+    public void pintarSegundoObstaculo(Graphics2D gObstaculo) {
+        gObstaculo.setColor(Color.green);
+        gObstaculo.fillRect(posicionSegundoObstaculoX, posicionSegundoObstaculoY, ancho, alto);
+    }
+
+    public void pintarTercerObstaculo(Graphics2D gObstaculo) {
+        gObstaculo.setColor(Color.white);
+        gObstaculo.fillRect(posicionObstaculoX, posicionTercerObstaculoY, anchoTercero, alto);
+    }
+
+    public Rectangle interseccionObstaculo() {
         return new Rectangle(posicionObstaculoX, posicionObstaculoY, ancho, alto);
+    }
+
+    public Rectangle interseccionSegundoObstaculo() {
+        return new Rectangle(posicionSegundoObstaculoX, posicionSegundoObstaculoY, ancho, alto);
+    }
+
+    public Rectangle interseccionTercerObstaculo() {
+        return new Rectangle(posicionObstaculoX, posicionTercerObstaculoY, anchoTercero, alto);
     }
 }
